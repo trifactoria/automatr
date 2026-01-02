7b0c4d8b51010224278be3a18cc24ff4971b522b
Andy <trifactoria@gmail.com>
2026-01-01 22:56:39 -0500

    updating prosody config

--- config/prosody/prosody.cfg.lua
+++ config/prosody/prosody.cfg.lua
@@ -16,7 +16,12 @@ modules_enabled = {
   "disco";
   "pep";
   "bookmarks";
+  "ping";
+  "smacks";
 }
+
+c2s_timeout = 300
+s2s_timeout = 300
 
 -- If you want message carbons later: add "carbons" and the xep module.
 -- For now keep minimal.
